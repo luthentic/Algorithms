be04fd84a9c97d36f688f158d1532d58f8f006a7
SEWOONG LEE <dltpdnd1234@gmail.com>
2024-01-23 17:18:56 -0500

    Test Commit

--- Main.tf
+++ Main.tf
@@ -18,3 +18,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
